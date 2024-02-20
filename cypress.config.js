8edb8d292befab5dfae179c82f005fad35c8e11e
Melissa Alpad <74080463+MelissaBaron-A@users.noreply.github.com>
2024-02-21 08:57:58 +1100

    update config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 6000,
+  pageLoadTimeout: 3000,
+  reporter: 'mochawesome',
+  retries: {
+    runMode: 1,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
